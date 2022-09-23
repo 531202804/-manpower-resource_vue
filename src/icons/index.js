94a06086e33ae9d769e3512f1b0739a3f51e2999
KUI <www.wjan531202804@qq.com>
2022-09-23 17:13:09 +0800

    feat:项目删减初始化完成

--- src/icons/index.js
+++ src/icons/index.js
@@ -5,5 +5,9 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
+// req 函数=>   根据路径       对应的模块 导入
 const requireAll = requireContext => requireContext.keys().map(requireContext)
+// map 返回一个新数组
+// 长度和原来一样
+// 映射
 requireAll(req)
